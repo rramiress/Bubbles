6c876e9c47de37806f109c7496b5b01f37e0848f
rramiress <rramiress1979@gmail.com>
2017-12-01 15:26:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person man = new Person(88, 16, "Vasya");
+        Person man = new Person(12, 28, "Yura");
+        Person man = new Person(7, 21, "Petya");
+        Person man = new Person(28, 8, "Volodya");
+        Person man = new Person(44, 12, "Styopa");
+        Person man = new Person(75, 28, "Max");
+        Person man = new Person(93, 56, "Kolya");
+        Person man = new Person(2, 43, "Oleg");
+        Person man = new Person(39, 22, "Roman");
+        Person man = new Person(61, 4, "Nazar");
+
+        System.out.println(man );
+
+    }
+        public void setPeople(Person[] )
+    ArrayList<Person> people = new ArrayList <>();
+
+
+}
